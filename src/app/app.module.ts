0950acd284dde11be7b7b0c225a7fd314c69f3c4
deepak <deepak@kreatio.com>
2018-11-04 19:50:42 +0530

    DI configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { InjectableRxStompConfig, RxStompService, rxStompServiceFactory } from '@stomp/ng2-stompjs';
+
 import { AppComponent } from './app.component';
+import { myRxStompConfig } from './my-rx-stomp.config';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: InjectableRxStompConfig,
+      useValue: myRxStompConfig
+    },
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+      deps: [InjectableRxStompConfig]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/my-rx-stomp.config.ts
@@ -0,0 +1,29 @@
+import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
+
+export const myRxStompConfig: InjectableRxStompConfig = {
+  // Which server?
+  brokerURL: 'ws://127.0.0.1:15674/ws',
+
+  // Headers
+  // Typical keys: login, passcode, host
+  connectHeaders: {
+    login: 'guest',
+    passcode: 'guest'
+  },
+
+  // How often to heartbeat?
+  // Interval in milliseconds, set to 0 to disable
+  heartbeatIncoming: 0, // Typical value 0 - disabled
+  heartbeatOutgoing: 0, // Typical value 20000 - every 20 seconds
+
+  // Wait in milliseconds before attempting auto reconnect
+  // Set to 0 to disable
+  // Typical value 5000 (5 seconds)
+  reconnectDelay: 200,
+
+  // Will log diagnostics on console
+  // It can be quite verbose, not recommended in production
+  debug: (msg: string): void => {
+    console.log(new Date(), msg);
+  }
+};
