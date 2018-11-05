fef182d5f47c3028b0718235d2de9355288f09f9
deepak <deepak@kreatio.com>
2018-11-05 08:55:14 +0530

    Stopping the watch

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RxStompService} from '@stomp/ng2-stompjs';
 import { Message } from '@stomp/stompjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, OnDestroy {
   public receivedMessages: string[] = [];
+  private topicSubscription: Subscription;
 
   constructor(private rxStompService: RxStompService) { }
 
   ngOnInit() {
-    this.rxStompService.watch('/topic/demo').subscribe((message: Message) => {
+    this.topicSubscription = this.rxStompService.watch('/topic/demo').subscribe((message: Message) => {
       this.receivedMessages.push(message.body);
     });
+  }
+
+  ngOnDestroy() {
+    this.topicSubscription.unsubscribe();
   }
 
   onSendMessage() {
